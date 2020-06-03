e54c2a6dbb4ea7af453cfc190df92ac0370c8acd
arlindomath <63853947+arlindomath@users.noreply.github.com>
2020-06-03 14:21:59 -0300

    Mapa de Calor usando a Biblioteca Folium
    O mapa de calor mostra quais são os pontos quentes dos dados. O exemplo que 
    utilizamos nesse código são os dados da Covid-19, usando o mapa de calor podemos identificar as regiões com maior concentração de casos da doença. Para construir esse Mapa utilizaremos a Biblioteca Folium - Acesse a documentação: <https://python-visualization.github.io/folium/>

---
+++ HeatMap.py
@@ -0,0 +1,14 @@
+import folium
+from folium.plugins import HeatMap
+import pandas as pd
+
+
+dados_covid19 = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-gps.csv')
+
+mapa = folium.Map (location=[-15.77972, -47.92972], zoom_start=4, min_zoom=4, control_scale=True)
+
+locais = dados_covid19 [["lat","lon"]].values.tolist()
+
+HeatMap (locais, radius=13).add_to(mapa)
+mapa.save('mapa.html')
+
