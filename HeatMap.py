c3294ed83446c0c018245aaf409e63a070bc8f60
arlindomath <63853947+arlindomath@users.noreply.github.com>
2020-06-03 14:49:46 -0300

    Update HeatMap.py

--- HeatMap.py
+++ HeatMap.py
@@ -2,7 +2,7 @@ import folium
 from folium.plugins import HeatMap
 import pandas as pd
 
-
+##Dados da Plataforma Brasil.io
 dados_covid19 = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-gps.csv')
 
 mapa = folium.Map (location=[-15.77972, -47.92972], zoom_start=4, min_zoom=4, control_scale=True)
